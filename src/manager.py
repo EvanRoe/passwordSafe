2d7de5b51b89047bd91ad8c663a84b8d8c4901c7
Evan <evan33r@gmail.com>
2026-01-22 00:48:06 +0100

    Added first time user, def main(), load_vault

--- src/manager.py
+++ src/manager.py
@@ -12,9 +12,33 @@ class PasswordManager:
     # initialising values
     def __init__(self, vault_file="vault.json"):
         self.vault_file = vault_file
-        self.vault = {}
+        self.vault_file = {}
         self.salt = None
         self.key = None
+
+    # set up 
+    def initialise(self):
+        if not os.path.exists(self.vault_file):
+            self.salt = os.urandom(16)
+
+            while True:
+                pw1 = getpass("Create a master password: ")
+                pw2 = getpass("Once more cause you're stupid: ")
+
+                if pw1 != pw2:
+                    print("They don't match, try again.")
+                    continue
+                if len(pw1) < 6:
+                    print("It has got to be longer bruh.")
+                    continue
+                break
+            
+            self.key = self.derive_key(pw1, self.salt)
+            self.vault = {}
+            self.save_vault()
+            print(f"New vault created at {self.vault_file}")
+            # clear password from memory
+            pw1 = pw2 = "x" * len(pw1)
 
     # using PBKDF2HMAC to "blend" the password iterations times with salt key
     def derive_key(self, password: str, salt: bytes) -> bytes:
@@ -36,6 +60,7 @@ class PasswordManager:
         f = Fernet(key)
         return f.decrypt(encrypted_data).decode()
     
+    # each password entry is a dict with other parts, kinda fun having all that data
     def _create_entry(self, service: str, username: str, password: str, notes="") -> dict:
         current_time = datetime.now()
         entry = {"service": service,
@@ -93,13 +118,61 @@ class PasswordManager:
         with open(self.vault_file, 'wb') as f: # 'wb' = write binary
             f.write(enc_bytes)
 
+    def load_vault(self):
+        if not os.path.exists(self.vault_file):
+            return False
+      
+        with open(self.vault_file, 'rb') as f:                
+            enc_bytes = f.read()
 
+        json_string = self.decrypt_data(enc_bytes, self.key)
+        data = json.loads(json_string)
+        # extract metadata
+        self.salt = base64.b64decode(data["metadata"]["salt"])
+
+        # convert vault data into bytes
+        self.vault = {}
+        for service, entry_dict in data["vault"].items():
+            self.vault[service] = {}
+            for name, value in entry_dict.items():
+                if name == "service":
+                    self.vault[service][name] = value
+                else:
+                    self.vault[service][name] = \
+                    base64.b64decode(value)
+
+    def load_metadata(self):
+        if not os.path.exists(self.vault_file):
+            return None
+        with open(self.vault_file, 'rb') as f:                
+            enc_bytes = f.read()
+
+        json_string = self.decrypt_data(enc_bytes, self.key)
+        data = json.loads(json_string)
+        # return salt
+        return data["metadata"]
 
 
 
 
     
-if __name__ == "__main__":
-    test_dict = {"gmail": {"user": "test@test.com", "pass": "mypass123"}}
-    json_str = json.dumps(test_dict, indent=1)
-    print(json_str)
+def main():
+    pm = PasswordManager()
+
+    if pm.initialise():
+        print("Setup complete.")
+    else:
+        password = getpass("Bruh type in the master: ")
+        # load salt from metadata
+        metadata = pm.load_metada()
+        pm.salt = base64.b64decode(metadata["salt"])
+        pm.key = pm.derive_key(password, pm.salt)
+
+        # load the vault
+        if pm.load_vault():
+            pass
+        else:
+            print("Failed to unlock the vault?! wHO aRE yOU?")
+            return
+    pm.run_menu()
+        
